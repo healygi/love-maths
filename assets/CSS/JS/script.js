c22f31863ec89d6812e07a9a889957e40bd42f88
healygi <healygi@tcd.ie>
2022-02-28 13:25:53 +0000

    add event listeners for button clicks and document loading

--- assets/CSS/JS/script.js
+++ assets/CSS/JS/script.js
@@ -1,31 +1,17 @@
-function runGame() {
+// Wait for the DOM to finish loading before running the game
+// Get the button elements and add event listeners to them
 
-}
+document.addEventListener("DOMContentLoaded", function() {
+    let buttons = document.getElementsByTagName("button");
 
-function checkAnswer() {
-
-}
-
-function calculateCorrectAnswer() {
-
-}
-
-function incrementScore() {
-
-}
-
-function incrementWrongAnswer() {
-
-}
-
-function displaySubtractQuestion() {
-
-}
-
-function displayAdditionQuestion() {
-
-}
-
-function displayMultiplyQuestion() {
-    
-}
+    for (let button of buttons) {
+        button.addEventListener("click", function() {
+            if (this.getAttribute("data-type") === "submit") {
+                alert("You clicked Submit!");
+            } else {
+                let gameType = this.getAttribute("data-type");
+                alert(`You clicked ${gameType}`);
+            }
+        })
+    }   
+})
