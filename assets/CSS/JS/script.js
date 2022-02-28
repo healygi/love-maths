5cdb95689c6f02706f25a25ea4feda9d4a6eb8e6
healygi <healygi@tcd.ie>
2022-02-28 12:15:51 +0000

    structure of game functions

---
+++ assets/CSS/JS/script.js
@@ -0,0 +1,31 @@
+function runGame() {
+
+}
+
+function checkAnswer() {
+
+}
+
+function calculateCorrectAnswer() {
+
+}
+
+function incrementScore() {
+
+}
+
+function incrementWrongAnswer() {
+
+}
+
+function displaySubtractQuestion() {
+
+}
+
+function displayAdditionQuestion() {
+
+}
+
+function displayMultiplyQuestion() {
+    
+}
