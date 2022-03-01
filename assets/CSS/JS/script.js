46676d5ecba21f7e9d4bed87c5c3f2a250430311
healygi <healygi@tcd.ie>
2022-03-01 12:53:06 +0000

    Add favicon and final touch ups for the user

--- assets/CSS/JS/script.js
+++ assets/CSS/JS/script.js
@@ -14,14 +14,23 @@ document.addEventListener("DOMContentLoaded", function() {
             }
         })
     }  
-    runGame("addition");
+document.getElementById("answer-box").addEventListener("keydown", function(event) {
+    if(event.key === "Enter") {
+        checkAnswer();
+    }
 })
+})
+
+runGame("addition");
 
 /** Main game "loop", called when the script is first loaded and 
  * after the users answer has been processed.
  * 
  */
 function runGame(gameType) {
+
+    document.getElementById('answer-box').value = "";
+    document.getElementById('answer-box').focus();
 
     //creates 2 random numbers between 1 and 25
     let num1 = Math.floor(Math.random()* 25) +1;
@@ -130,4 +139,5 @@ function displayMultiplyQuestion(operand1, operand2) {
     document.getElementById('operand1').textContent = operand1;
     document.getElementById('operand2').textContent = operand2;
     document.getElementById('operator').textContent = "x";
+
 }
