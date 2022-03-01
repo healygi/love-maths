66812a2770aeb10b3c50de2551bd3a9a92657897
healygi <healygi@tcd.ie>
2022-03-01 07:48:18 +0000

    Add code to check answer and respond to the user.

--- assets/CSS/JS/script.js
+++ assets/CSS/JS/script.js
@@ -35,8 +35,23 @@ function runGame(gameType) {
     }
 }
 
+/** Check the answer against the first element 
+ * in the returned calculateCorrectAnswer array.
+ * 
+ */
+
 function checkAnswer() {
 
+    let userAnswer = parseInt(document.getElementById("answer-box").value);
+    let calculatedAnswer = calculateCorrectAnswer();
+    let isCorrect = userAnswer === calculatedAnswer[0];
+
+    if (isCorrect) {
+        alert ("Hey! You answered correctly! :D")
+    } else {
+        alert (`Awwww...you answered ${userAnswer}. The correct answer was ${calculatedAnswer[0]}!`)
+    }
+runGame(calculatedAnswer[1]);
 }
 
 /**
